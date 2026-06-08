921576284ce9d1405cd4fd31da6f9ba158f94cd8
eshen <etiennewayne@gmail.com>
2026-06-09 07:46:06 +0800

    update rpeo

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -66,8 +66,8 @@ class AppServiceProvider extends ServiceProvider
 
         ]);
 
-        // if (app()->environment('production')) {
-        //     URL::forceScheme('https');
-        // }
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
