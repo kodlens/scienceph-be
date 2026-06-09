24bb88bf4e386cb0c91427947da3d4fd52b81232
eshen <etiennewayne@gmail.com>
2026-06-09 10:26:01 +0800

    update api

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,9 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::get('/test', function (Request $request) {
+    return response()->json(['message' => 'API is running']);
+});
 
 
 /* ============================
